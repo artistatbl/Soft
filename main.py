a3f8f242956b34ccef39cd015a7c544e6cfa8e4e
artistatbl <jdaly2991@gmail.com>
2023-06-15 20:07:33 +0100

    first commit

--- main.py
+++ main.py
@@ -5,13 +5,15 @@ import asyncio
 import discord
 from discord.ext import commands
 from cogs.event import Event
+from cogs.music import Music
+
 
 with open('config.json') as f:
     config = json.load(f)
 
 intents=  discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix="$", intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 async def load():
     for filename in os.listdir('./cogs'):
@@ -24,12 +26,15 @@ async def load():
 
 @bot.event
 async def on_ready():
-    await bot.change_presence(status=discord.Status.online, activity=discord.Game('$help'))
+    activity = discord.Activity(name='!help', type=discord.ActivityType.listening)
+    await bot.change_presence(status=discord.Status.online, activity=activity)
+   # await bot.change_presence(status=discord.Status.online, activity=discord.Game('!help'))
     guilds = [guild.name for guild in bot.guilds]
     print(f"Connected to {len(guilds)} guilds: {', '.join(guilds)}")
     print(f'{bot.user.name} has connected to Discord!')
     print("---------------------------------------")
     await bot.add_cog(Event(bot))
+    await bot.add_cog(Music(bot))
   
 
 
